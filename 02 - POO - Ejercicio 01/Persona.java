20d5bc14790f852f8b20b3f72c7423ffa78abcee
Oscar Loaiza <oaloaiza9@misena.edu.co>
2023-03-24 09:31:47 -0500

    Ejercicio 01 - Creado

---
+++ 02 - POO - Ejercicio 01/Persona.java
@@ -0,0 +1,72 @@
+
+public class Persona{
+
+	int documento;
+	String nombres;
+	String apellidos;
+	String direccion;
+	String telefono;
+	String email;
+
+	public Persona(int documento, String nombres, String apellidos, String direccion, String telefono, String email){
+		this.documento = documento;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.email = email;
+	}
+
+	public String toString(){
+		return this.documento+" || "+this.nombres+" "+this.apellidos;
+	}
+
+	public int getDocumento(){
+		return this.documento;
+	}
+
+	public String getNombres(){
+		return this.nombres;
+	}
+
+	public String getApellidos(){
+		return this.apellidos;
+	}
+
+	public String getDireccion(){
+		return this.direccion;
+	}
+
+	public String getTelefono(){
+		return this.telefono;
+	}
+
+	public String getEmail(){
+		return this.email;
+	}
+
+	public void setDocumento(int documento){
+		this.documento = documento;
+	}
+
+	public void setNombres(String nombres){
+		this.nombres = nombres;
+	}
+
+	public void setApellidos(String apellidos){
+		this.apellidos = apellidos;
+	}
+
+	public void setDireccion(String direccion){
+		this.direccion = direccion;
+	}
+
+	public void setTelefono(String telefono){
+		this.telefono = telefono;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+}
---
+++ 02 - POO - Ejercicio 01/Principal.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Principal{
+	public static void main(String[] args) {
+		
+		Scanner entrada_numero = new Scanner(System.in);
+		Scanner entrada_texto = new Scanner(System.in);
+
+		Persona arreglo [] = new Persona[99];
+		int posicion = 0;
+		int opcion = 0;
+
+		do{
+			System.out.println("\n");
+			System.out.println("||----------------------------------------||");
+			System.out.println("||--------------- PERSONAS ---------------||");
+			System.out.println("||----------------------------------------||");
+			System.out.println("||               Personas Registradas: "+((posicion<10)? "0":"")+posicion+" ||");
+			System.out.println("||                                        ||");
+			System.out.println("||  1. Registrar Persona.                 ||");
+			System.out.println("||  2. Ver lista de Personas.             ||");
+			System.out.println("||  3. Ordenar lista Personas.            ||");
+			System.out.println("||  4. Salir.                             ||");
+			System.out.println("||----------------------------------------||");
+			System.out.print("||=> Ingrese una opcion: ");
+			opcion = entrada_numero.nextInt();
+
+			if( opcion==1 ){
+				// Pedir los datos para la creacion de la persona
+				System.out.print(" => Ingrese Documento: ");
+				int documento = entrada_numero.nextInt();
+
+				System.out.print(" => Ingrese Nombres: ");
+				String nombres = entrada_texto.nextLine();
+
+				System.out.print(" => Ingrese Apellidos: ");
+				String apellidos = entrada_texto.nextLine();
+
+				System.out.print(" => Ingrese Direccion: ");
+				String direccion = entrada_texto.nextLine();
+
+				System.out.print(" => Ingrese Telefono: ");
+				String telefono = entrada_texto.nextLine();
+
+				System.out.print(" => Ingrese Email: ");
+				String email = entrada_texto.nextLine();
+
+				// Crear un objeto Persona
+				Persona temporal = new Persona(documento, nombres, apellidos, direccion, telefono, email);
+
+				// Guardar el objeto en el Arreglo
+				arreglo[posicion] = temporal;
+
+				// aumentar el contador
+				posicion++;
+			}else if( opcion==2 ){
+
+				System.out.println(" => LISTA DE PERSONAS: ");
+				for (int i=0; i<arreglo.length; i++) {
+					if(arreglo[i]!=null){
+						System.out.println( "     => "+arreglo[i].toString() );
+					}else{
+						break;
+					}
+				}
+
+			}else if( opcion==3 ){
+				
+			}else if( opcion==4 ){
+				System.out.println(" == Saliendo ==");
+			}else{
+				System.out.println(" == Opcion Invalida ==");
+			}
+
+		}while( opcion!=4 );
+
+	}
+}
