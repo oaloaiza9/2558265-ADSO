bd9f3c69f962d738a1ef8b18a5bd2e6400e938e8
Oscar Loaiza <oaloaiza9@misena.edu.co>
2023-04-21 10:11:28 -0500

    Ajuste de nombres Triki.java

--- 03 - POO - Ejercicio 02/Principal.java
+++ 03 - POO - Ejercicio 02/Principal.java
@@ -2,7 +2,7 @@
 public class Principal{
 	public static void main(String[] args) {
 		
-		Triki partidas = new Triki("Jeffry", "Mauro");
+		Triki partidas = new Triki("Nombre 01", "Nombre 02");
 		partidas.start();
 		
 	}
