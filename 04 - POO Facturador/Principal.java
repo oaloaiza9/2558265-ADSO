337a3cf301a8804981b17998057ad2d9dcbe85eb
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-04-28 10:32:21 -0500

    Descripcion de la actividad de Facturas

--- 04 - POO Facturador/Principal.java
+++ 04 - POO Facturador/Principal.java
@@ -2,7 +2,32 @@
 public class Principal{
 	public static void main(String[] args) {
 		
-		System.out.println(" -- Codigo para Ejercicio del Facturador. "); 
+		System.out.println(" -- Codigo para Ejercicio del Facturador. ");
+		/*  Realizar una aplicacion en JAVA para generar faturas de venta de
+			productos, con las siguientes consideraciones:
+
+			- Debe exister un menu principal para definir las funcionalidades 
+			  principales como:
+			  -> Creacion de cliente
+			  -> Creacion de Vendedor
+			  -> Creacion de Factura
+			  -> Ver lista de Clientes
+			  -> Ver lista de Vendedores
+			  -> Ver lista de Facturas
+			  -> Ver detalle de Factura
+
+			- En la creacion de nueva Factura se debe seleccionar un cliente
+			  para la factura, seleccionar un vendedor e ingresar los productos
+			  a la factura con su respectiva cantidad de venta. 
+			  Al finalizar el ingreso de los productos se debe mostrar en pantalla
+			  el total facturado.
+			
+			- La lista de Facturas debe tener el siguiente formato:
+			  FECHA => ID_FACTURA => VENDEDOR => CLIENTE => TOTAL
+
+			- El detalle de la Factura debe visualizarse con un formato definido
+			  por ustedes, donde se incluya toda la informacion necesaria.
+		*/
 
 	}
 }
