6fc512ae55fd92185487cb85c772265576bfd5f1
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-11-20 09:37:04 -0500

    Agregando ejemplo de consumo API

---
+++ 22 - Android - Consumo API/SegundaAplicacion/app/src/main/java/com/example/segundaaplicacion/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.segundaaplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgInternet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_main ) ;
+
+        imgInternet = findViewById(R.id.imgInternet);
+    }
+
+    public void consumoGetJson(View vista){
+        System.out.println("Iniciando consumo");
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://172.20.10.2:80/APIenPHP/Obtener.php";
+
+        JsonObjectRequest solicitud =  new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println("El servidor responde OK");
+                System.out.println(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("El servidor responde con un error:");
+                System.out.println(error.getMessage());
+            }
+        });
+
+        queue.add(solicitud);
+    }
+
+    public void consumoPostJson(View vista){
+        System.out.println("Iniciando consumo");
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://172.20.10.2:80/APIenPHP/Insert.php";
+
+        StringRequest solicitud =  new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    System.out.println("El servidor POST responde OK");
+                    JSONObject jsonObject = new JSONObject(response);
+                    System.out.println(jsonObject.toString());
+                } catch (JSONException e) {
+                    System.out.println("El servidor POST responde con un error:");
+                    System.out.println(e.getMessage());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("El servidor POST responde con un error:");
+                System.out.println(error.getMessage());
+            }
+        }){
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("cedula", "107701");
+                params.put("nombres", "ANDROID");
+                params.put("apellidos", "STUDIO");
+                params.put("direccion", "Carrera 8va");
+                params.put("telefono", "3000001");
+                params.put("email", "android@mail.com");
+                return params;
+            }
+        };
+
+        queue.add(solicitud);
+    }
+
+    public void consumoImagen(View vista){
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png";
+
+        ImageRequest solicitud = new ImageRequest(
+                url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        // Set the image in the ImageView.
+                        imgInternet.setImageBitmap(bitmap);
+                    }
+                },
+                0, 0, null, // maxWidth, maxHeight, decodeConfig;
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle the error.
+                    }
+                });
+
+        queue.add(solicitud);
+    }
+
+}
